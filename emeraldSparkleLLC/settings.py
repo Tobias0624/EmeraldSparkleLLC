c5322f819ab5417be71cd53cbf689042460718d3
Tobias0624 <tobi.adeyeye@gmail.com>
2021-11-25 02:32:02 -0800

    Created template folderwith base.html and index.html, redirected project urls, created and registered index view, configured static in setting

--- emeraldSparkleLLC/settings.py
+++ emeraldSparkleLLC/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from decouple import config
 
-SECRET_KEY = config('SECRET_KEY')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#j)a0&2*zdhvvyvx4)@v5e2nh!3###mvdh)pc-ck8cfaqf8d()'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
@@ -127,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
